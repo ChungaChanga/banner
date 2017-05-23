22be55b0906e7d01df0019adeda885dafcc3de78
AndreyTulin <aatuline@gmail.com>
2017-05-23 13:28:30 +0400

    2 commit

--- bannerTiberis.js
+++ bannerTiberis.js
@@ -1,6 +1,6 @@
 var targetID = 'banner_container';//ID блока для вставки баннера
 var HTML = '<div id="bannerTiberis">\
-              <p>Привет, я баннер</br><img src="https://cdn.rawgit.com/ChungaChanga/banner/08391075/image.png"></p>\
+              <p>Привет, я баннер</br><img src="https://cdn.rawgit.com/ChungaChanga/banner/991a157c/image.png"></p>\
             </div>';//HTML баннера
 var CSS = '#' + targetID +' p{font-size: 40pt; text-align: center;} #' + 
                 targetID + ' img{width: 600px;} #bannerTiberis{margin:auto;}';// CSS баннера
