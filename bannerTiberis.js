d6abcfa8834caeabf66e2bf71e7fd203ad2d5c2f
AndreyTulin <aatuline@gmail.com>
2017-05-23 14:21:04 +0400

    2 commit

--- bannerTiberis.js
+++ bannerTiberis.js
@@ -7,14 +7,14 @@ targetTag.id = targetID;
 var HTML = '<div id="bannerTiberis">\
               <p>Привет, я баннер</br>\
                 <a href="https://www.tiberis.ru/">\
-                  <img src="https://cdn.rawgit.com/ChungaChanga/banner/991a157c/image.png"></p>\
+                  Ссылка\
                 </a>\
                 <a href="https://www.tiberis.ru/">\
                   <img src="https://cdn.rawgit.com/ChungaChanga/banner/991a157c/image.png"></p>\
                 </a>\
             </div>';//HTML баннера
 var CSS = '#' + targetID +' p{font-size: 40pt; text-align: center;} #' + 
-                targetID + ' img{width: 1108px;height:140px;} #bannerTiberis{margin:auto;width:1108px;height:140px;border:2px solid;}';// CSS баннера
+                targetID + ' img{width: 300px;height:40px;} #bannerTiberis{margin:auto;width:1108px;height:140px;border:2px solid;}';// CSS баннера
 
 var bannerTag = document.createElement('div');
 var CSSTag = document.createElement('style');
